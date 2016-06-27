dcfee19849b90e7a7b7184ea3f27e514208f072e
rsdu <duruishan@yeah.net>
2016-06-27 15:56:10 +0800

    first commit

---
+++ app/src/main/java/com/durs/mydraw/LayerView.java
@@ -0,0 +1,18 @@
+package com.durs.mydraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by durs on 2016/6/27.
+ */
+public class LayerView extends View {
+
+    public LayerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+
+}
